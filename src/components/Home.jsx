ce090209275bcf58b5b7e2fd7362d2574bdadfdb
npmisantosh <santoshhumagain1997@gmail.com>
2024-07-24 02:43:47 -0500

    Working Return to home, need to fix retry

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,47 +4,47 @@ import image from "../assets/tradeimg.GIF";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const navigate = useNavigate();
-  const levels = [
-    {
-      image: "imagelink",
-      title: "Beginner",
-      desc: "For those who are just starting out",
-      click: "easy",
-    },
-    {
-      image: "imagelink",
-      title: "Expert",
-      desc: "Read the charts like the back of their hand",
-      click: "hard",
-    },
-  ];
+	const navigate = useNavigate();
+	const levels = [
+		{
+			image: "imagelink",
+			title: "Beginner",
+			desc: "For those who are just starting out",
+			click: "easy",
+		},
+		{
+			image: "imagelink",
+			title: "Expert",
+			desc: "Read the charts like the back of their hand",
+			click: "hard",
+		},
+	];
 
-  const handleButton = (item) => {
-    navigate("/quiz", { state: { level: item.click } });
-  };
+	const handleButton = (item) => {
+		navigate("/quiz", { state: { level: item.click } });
+	};
 
-  return (
-    <div className="home-container">
-      <div className="title">Trading Quiz</div>
-      <div className="bb">
-        <img src={image} className="level-picture" alt="tradinggif" />
-        {levels.map((item, index) => (
-          <button
-            key={index}
-            className="level-container"
-            onClick={() => handleButton(item)}
-          >
-            <div className="imaheholder"></div>
-            <div className="textholder">
-              <div className="title">{item.title}</div>
-              <div className="description">{item.desc}</div>
-            </div>
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="home-container">
+			<div className="title">Trading Quiz</div>
+			<div className="bb">
+				<img src={image} className="level-picture" alt="tradinggif" />
+				{levels.map((item, index) => (
+					<button
+						key={index}
+						className="level-container"
+						onClick={() => handleButton(item)}
+					>
+						<div className="imaheholder"></div>
+						<div className="textholder">
+							<div className="title">{item.title}</div>
+							<div className="description">{item.desc}</div>
+						</div>
+					</button>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Home;
