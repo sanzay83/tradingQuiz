24cc17c645c197d76c5c1b40d4030d48b83ea705
Sanjay Duwal <sanzay8342258@gmail.com>
2024-07-26 18:03:32 -0500

    added changes on home buttons images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,30 +3,32 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Quiz from "./Quiz";
 import Result from "./Result";
+import Study from "./Study";
 import SplashScreen from "./Splashscreen";
 
 function App() {
-	const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-	useEffect(() => {
-		setTimeout(() => {
-			setIsLoading(false);
-		}, 4000);
-	}, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
 
-	return (
-		<>
-			{isLoading ? (
-				<SplashScreen />
-			) : (
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/quiz" element={<Quiz />} />
-					<Route path="/results" element={<Result />} />
-				</Routes>
-			)}
-		</>
-	);
+  return (
+    <>
+      {isLoading ? (
+        <SplashScreen />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/results" element={<Result />} />
+          <Route path="/study" element={<Study />} />
+        </Routes>
+      )}
+    </>
+  );
 }
 
 export default App;
