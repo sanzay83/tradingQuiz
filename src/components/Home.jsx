04c117c2ba2b3328108d372c5d10acbcdb030fc5
npmisantosh <santoshhumagain1997@gmail.com>
2024-07-26 13:56:19 -0500

    some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "../styles/Home.scss";
 import image from "../assets/tradeimg.GIF";
+import easyimage from "../assets/easy.png";
+import hardimage from "../assets/hard.png";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
 	const navigate = useNavigate();
 	const levels = [
 		{
-			image: "imagelink",
+			image: easyimage,
 			title: "Beginner",
 			desc: "For those who are just starting out",
 			click: "easy",
 		},
 		{
-			image: "imagelink",
+			image: hardimage,
 			title: "Expert",
 			desc: "Read the charts like the back of their hand",
 			click: "hard",
@@ -35,7 +37,13 @@ function Home() {
 						className="level-container"
 						onClick={() => handleButton(item)}
 					>
-						<div className="imaheholder"></div>
+						<div className="imaheholder">
+							<img
+								src={item.image}
+								className="level-picture"
+								alt="tradinggif"
+							/>
+						</div>
 						<div className="textholder">
 							<div className="title">{item.title}</div>
 							<div className="description">{item.desc}</div>
