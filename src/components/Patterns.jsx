5cc1dd6335dde056b33af7e446c77a27b3d7c24f
Sanjay Duwal <sanzay8342258@gmail.com>
2024-07-28 16:49:45 -0500

    fixed the randomized splash screen

--- src/components/Patterns.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Patterns() {
-	return <div>Study</div>;
-}
-
-export default Patterns;
--- src/components/Splashscreen.jsx
+++ src/components/Splashscreen.jsx
@@ -1,45 +1,50 @@
 import React, { useState, useEffect } from "react";
-import "../styles/Splashscreen.scss"; // Import SCSS file for styles
+import "../styles/Splashscreen.scss";
 
 function Splashscreen() {
-	const loadingPhrases = [
-		"Waking the Europe Markets",
-		"Ringing the Wall Street Bell",
-		"Borrowing from the Fed",
-		"Checking if the bulls are awake",
-		"Chasing after high-frequency traders",
-		"Analyzing Bitcoin volatility",
-		"Measuring market sentiment",
-		"Trading memes on Reddit",
-		"Waving goodbye to the bears",
-		"Counting stock market crashes",
-		"Calculating P/E ratios",
-		"Hodling through the dip",
-		"Buying the rumor, selling the news",
-		"Selling options like a boss",
-		"Stalking Elon Musk's tweets",
-		"Catching falling knives",
-		"Watching the ticker tape",
-	];
+  const loadingPhrases = [
+    "Waking the Europe Markets",
+    "Ringing the Wall Street Bell",
+    "Borrowing from the Fed",
+    "Checking if the bulls are awake",
+    "Chasing after high-frequency traders",
+    "Analyzing Bitcoin volatility",
+    "Measuring market sentiment",
+    "Trading memes on Reddit",
+    "Waving goodbye to the bears",
+    "Counting stock market crashes",
+    "Calculating P/E ratios",
+    "Hodling through the dip",
+    "Buying the rumor, selling the news",
+    "Selling options like a boss",
+    "Stalking Elon Musk's tweets",
+    "Catching falling knives",
+    "Watching the ticker tape",
+  ];
 
-	const [loadingText, setLoadingText] = useState(loadingPhrases[0]);
+  const randomNumber = () => {
+    return Math.floor(Math.random() * loadingPhrases.length);
+  };
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			const randomIndex = Math.floor(Math.random() * loadingPhrases.length);
-			setLoadingText(loadingPhrases[randomIndex]);
-		}, 1000); // Adjust the interval to change text every 2 seconds
+  const [loadingText, setLoadingText] = useState(
+    loadingPhrases[randomNumber()]
+  );
 
-		return () => {
-			clearInterval(interval);
-		};
-	}, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoadingText(loadingPhrases[randomNumber()]);
+    }, 1000);
 
-	return (
-		<div className="splash-screen">
-			<div className="loading-text">{loadingText}</div>
-		</div>
-	);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className="splash-screen">
+      <div className="loading-text">{loadingText}</div>
+    </div>
+  );
 }
 
 export default Splashscreen;
