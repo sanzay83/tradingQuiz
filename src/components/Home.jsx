c5fe457097e0a59b7b6dca4a0101ed65b465076a
npmisantosh <santoshhumagain1997@gmail.com>
2024-07-29 15:57:44 -0500

    icons for tudy page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,66 +3,67 @@ import "../styles/Home.scss";
 import image from "../assets/tradeimg.GIF";
 import easyimage from "../assets/easy.png";
 import hardimage from "../assets/hard.png";
+import studyimage from "../assets/study.png";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const navigate = useNavigate();
-  const levels = [
-    {
-      image: easyimage,
-      title: "Beginner",
-      desc: "For those who are just starting out",
-      click: "easy",
-    },
-    {
-      image: hardimage,
-      title: "Expert",
-      desc: "Read the charts like the back of their hand",
-      click: "hard",
-    },
-    {
-      image: "study",
-      title: "Study Material",
-      desc: "Read about the charts and candles",
-      click: "study",
-    },
-  ];
+	const navigate = useNavigate();
+	const levels = [
+		{
+			image: easyimage,
+			title: "Beginner",
+			desc: "For those who are just starting out",
+			click: "easy",
+		},
+		{
+			image: hardimage,
+			title: "Expert",
+			desc: "Read the charts like the back of their hand",
+			click: "hard",
+		},
+		{
+			image: studyimage,
+			title: "Study Material",
+			desc: "Read about the charts and candles",
+			click: "study",
+		},
+	];
 
-  const handleButton = (item) => {
-    if (item.click === "study") {
-      navigate("/study");
-    } else {
-      navigate("/quiz", { state: { level: item.click } });
-    }
-  };
+	const handleButton = (item) => {
+		if (item.click === "study") {
+			navigate("/study");
+		} else {
+			navigate("/quiz", { state: { level: item.click } });
+		}
+	};
 
-  return (
-    <div className="main-container">
-      <div className="title">Trading Quiz</div>
-      <div className="bb">
-        <img src={image} className="level-picture" alt="tradinggif" />
-        {levels.map((item, index) => (
-          <button
-            key={index}
-            className="level-container"
-            onClick={() => handleButton(item)}
-          >
-            <div className="imageholder">
-              <img
-                src={item.image}
-                className="level-picture"
-                alt="tradinggif"
-              />
-            </div>
-            <div className="textholder">
-              <div className="title">{item.title}</div>
-              <div className="description">{item.desc}</div>
-            </div>
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="main-container">
+			<div className="title">Trading Quiz</div>
+			<div className="bb">
+				<img src={image} className="level-picture" alt="tradinggif" />
+				{levels.map((item, index) => (
+					<button
+						key={index}
+						className="level-container"
+						onClick={() => handleButton(item)}
+					>
+						<div className="imageholder">
+							<img
+								src={item.image}
+								className="level-picture"
+								alt="tradinggif"
+							/>
+						</div>
+						<div className="textholder">
+							<div className="title">{item.title}</div>
+							<div className="description">{item.desc}</div>
+						</div>
+					</button>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Home;
