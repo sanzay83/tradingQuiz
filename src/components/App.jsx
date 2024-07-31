2021b4eb8ae3490496e30c553b2699810028551a
Sanjay Duwal <sanzay8342258@gmail.com>
2024-07-31 01:02:07 -0500

    remove Study and cleared readme page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Quiz from "./Quiz";
 import Result from "./Result";
-import Study from "./Study";
 import StudyEachItem from "./StudyEachItem";
 import SplashScreen from "./Splashscreen";
 import StudyItemDetails from "./StudyItemDetails";
@@ -27,7 +26,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/quiz" element={<Quiz />} />
           <Route path="/results" element={<Result />} />
-          <Route path="/study" element={<Study />} />
           <Route path="/studyEach" element={<StudyEachItem />} />
           <Route path="/studyitemdetails" element={<StudyItemDetails />} />
           <Route path="/progress" element={<Progress />} />
