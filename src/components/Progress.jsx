950a7d0f1081208cb62e963946ab434a50ce5871
npmisantosh <santoshhumagain1997@gmail.com>
2024-08-01 05:12:57 -0500

    working localStorage, incomplete progress page

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "../styles/Home.scss";
+import "../styles/Progress.scss";
 
 function Progress() {
-  return <div>Progress</div>;
+  let total_attempts = +localStorage.getItem("total_attempt") * 10;
+  let total_correct = localStorage.getItem("total_correct");
+  let conditional_text = (total_correct / total_attempts) * 100 + "%";
+
+  return (
+    <div className="main-container">
+      <div className="row-container avatar">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <div className="row-container progress">
+        <div width={conditional_text} className="progressbar green">
+          {total_correct}
+        </div>
+        <div className="progressbar red">{total_attempts}</div>
+      </div>
+      <p>
+        Out of {total_attempts} trades, you managed to get {total_correct}{" "}
+        trades right. That leaves you with a win percentage of{" "}
+        {(total_correct / total_attempts) * 100}%
+      </p>
+      <p>{conditional_text}</p>
+    </div>
+  );
 }
 
 export default Progress;
