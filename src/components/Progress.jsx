604b15bc0748732c57e2b37e1bbaaca69bcadad0
npmisantosh <santoshhumagain1997@gmail.com>
2024-08-01 21:30:46 -0500

    icon design for progress page

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "../styles/Home.scss";
 import "../styles/Progress.scss";
 
+import low_image from "../assets/low.png";
+import mid_image from "../assets/mid.png";
+import high_image from "../assets/high.png";
+
 function Progress() {
   let total_attempts = +localStorage.getItem("total_attempt") * 10;
   let total_correct = localStorage.getItem("total_correct");
@@ -9,16 +13,25 @@ function Progress() {
 
   return (
     <div className="main-container">
-      <div className="row-container avatar">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+      <div className="row-container">
+        <div>
+          {" "}
+          <img src={low_image} />
+        </div>
+        <div>
+          {" "}
+          <img src={mid_image} />
+        </div>
+        <div>
+          {" "}
+          <img src={high_image} />
+        </div>
       </div>
       <div className="row-container progress">
         <div width={conditional_text} className="progressbar green">
           {total_correct}
         </div>
-        <div className="progressbar red">{total_attempts}</div>
+        <div className="progressbar red">{total_attempts - total_correct}</div>
       </div>
       <p>
         Out of {total_attempts} trades, you managed to get {total_correct}{" "}
